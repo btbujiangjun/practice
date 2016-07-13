91249c3265ba019736b7576e87d10357991c6b97
btbujiangun <jiangjun4@sina.com>
2016-07-13 19:41:05 +0800

    update ConfigLoader.h

--- src/ConfigLoader.h
+++ src/ConfigLoader.h
@@ -5,28 +5,56 @@
 
 #ifndef PRACTICE_CONFIGLOADER_H
 #define PRACTICE_CONFIGLOADER_H
+
+#include <string>
+
+#define DISALLOW_COPY_AND_ASSIGN(TypeName)\
+		TypeName(const TypeName&);\
+		TypeName& operator=(const TypeName&)
+
 namespace practice{
+
 enum FieldType{
-	BUILT-IN = 1,
+	BUILT_IN = 1,
 	POINTER,
 	ARRAY,
 	FIELDTYPE_COUNT
 };
+
 struct FieldInfo{
 	std::string field_name;
 	std::string field_class;
 	FieldType field_type;
 };
+
 class ConfigLoader{
-	public:
-		explicit ConfigLoader(const std::string& config_name) : _config_name(config_name) {}
-		~ConfigLoader();
-		int init();
-		FieldInfo* field_config(int* field_size);
-	private:
-		std::string _config_name;
-		int _field_size;
-		FieldInfo* _fields;
+public:
+	//use default symbol ":" and "#"
+    explicit ConfigLoader(const std::string& file_name) : 
+			_file_name(file_name), \
+			_delimiter(":"), \
+			_commit("#") {};
+	//customized delimiter symbol and commit symbol
+	ConfigLoader(const std::string& file_name,\
+			const std::string& delimiter,\
+			const std::string& commit):
+			_file_name(file_name), \
+			_delimiter(delimiter), \
+			_commit(commit) {};
+	~ConfigLoader();
+
+    int load();
+
+	//FieldInfo* field_config(int* field_size);
+private:
+	void trim(std::string& io_str);
+
+	std::string _file_name;
+	std::string _delimiter;
+	std::string _commit;
+
+	FieldInfo* _fields;
+	//DISALLOW_COPY_AND_ASSIGN(ConfigLoader);
 };// ConfigLoader
 } // practice
 #endif //PRACTICE_CONFIGLOADER_H
