cd8fb85397ed3e1e7a62d8ba01204dbdc50815c3
btbujiangun <jiangjun4@sina.com>
2016-08-30 12:11:35 +0800

    update Configloader_test

--- test/ConfigLoader_test.cpp
+++ test/ConfigLoader_test.cpp
@@ -1,9 +1,12 @@
 #include "../src/ConfigLoader.h"
+#include "glog/logging.h"
 #include <iostream>
 
 using namespace practice;
 
-int main(){
+int main(int argc, char* argv[]){
+	google::InitGoogleLogging(argv[0]);
+	LOG(INFO) << "Init Config Loader.";
 	ConfigLoader* cl = new ConfigLoader("../conf/dict.conf");
 	int ret = cl->load();
 	if(ret == -2){
